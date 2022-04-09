34eaa615c17508d936be9e2522b61d3a240ccfbf
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-09 21:37:20 +0300

    Validate form and add Ngmodule for forms on App module

--- src/app/quoteform/quoteform.component.ts
+++ src/app/quoteform/quoteform.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { formatCurrency } from '@angular/common';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
+// import * as EventEmitter from 'events';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quoteform',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quoteform.component.css']
 })
 export class QuoteformComponent implements OnInit {
+  newQuote = new Quote("","","",);
+  
+  @Output() addQuote = new EventEmitter<Quote>();
+ 
+  submitQuote(f:NgForm){
+    if(f.valid){
+      this.addQuote.emit(this.newQuote);
+      this.newQuote = new Quote("","","",);
+    }else{
+      alert("Please enter valid inputs")
+    }
+
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
