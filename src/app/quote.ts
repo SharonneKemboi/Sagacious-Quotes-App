8c0d2cce27c10ae81993ebcedcd8379f970352cd
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-04-10 02:30:22 +0300

    Auto-generated commit [ci skip]

--- src/app/quote.ts
+++
@@ -1,5 +0,0 @@
-export class Quote {
-  constructor(public name:string,public author:string, public description:string,public likes:number,public dislikes:number, public datePosted:Date){
-      
-  }
-}
